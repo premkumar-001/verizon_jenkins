338b68218ef1c1c885b2f00fbb9e9e37f6957ffc
Prem <premkumarofficil@gmail.com>
2023-07-26 14:13:21 +0530

    initial commit

---
+++ src/test/java/automation/jenkinspractice/MobileTest.java
@@ -0,0 +1,19 @@
+package automation.jenkinspractice;
+
+import org.testng.annotations.Test;
+
+
+public class MobileTest 
+{
+    @Test
+    void login()
+    {
+        System.out.println("login");
+    }
+    @Test
+     void logout()
+    {
+        System.out.println("logout");
+       // int num =10/0;
+    }
+}
